a357e2a135488b264f4b0b76605cb22c03a0a2bb
AukarF <aukarafrank@gmail.com>
2025-01-31 21:48:54 -0400

    Initial commit

---
+++ Assets/Scripts/LerpHelper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LerpHelper : MonoBehaviour
+{
+    public Transform target;
+    public float lerpSpeed = 1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position,lerpSpeed * Time.deltaTime);
+    }
+}
