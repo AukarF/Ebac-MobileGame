eb9286d99928f61123ba630d4f9dc167677c5be1
AukarF <aukarafrank@gmail.com>
2025-02-06 15:10:28 -0400

    Added a Color coroutine to LevelManager Script

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-
+using DG.Tweening;
 public class LevelManager : MonoBehaviour
 {
     public Transform container;
@@ -19,6 +19,11 @@ public class LevelManager : MonoBehaviour
     private LevelPieceBasedSetup _currentSetup;
 
     private List<LevelPieceBase> _spawnedPieces =  new List<LevelPieceBase>();
+
+    [Header("Animation")]
+    public float scaleDuration = .2f;
+    public float scaleTimeBetweenPieces = .1f;
+    public Ease ease = Ease.OutBack;
 
     private void Awake()
     {
@@ -86,9 +91,26 @@ public class LevelManager : MonoBehaviour
 
 
         ColorManager.Instance.ChangeColorByType(_currentSetup.artType);
-    
+
+        StartCoroutine(ScalePiecesByType());
+
     }
 
+    IEnumerator ScalePiecesByType()
+    {
+        foreach (var p in _spawnedPieces)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        yield return null;
+
+        for (int i = 0; i < _spawnedPieces.Count; i++)
+        {
+            _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
+        }
+    }
 
     private void CreateLevelPiece(List<LevelPieceBase> list)
     {
